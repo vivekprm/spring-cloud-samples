6b2852a56e0b374ee4cff3927f78146743f71784
Vivek Mishra <vivek.pr.mi@gmail.com>
2017-05-20 09:01:13 +0530

    Config client configs

---
+++ configclient/src/main/java/com/client/ConfigClient.java
@@ -0,0 +1,14 @@
+package com.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by netstorm on 20/5/17.
+ */
+@SpringBootApplication
+public class ConfigClient {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient.class, args);
+    }
+}
